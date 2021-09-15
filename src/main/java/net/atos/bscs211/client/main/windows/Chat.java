700014cf56715d2db1c382c162a0b2de8c069332
RealSocius <mineplayer300guy@gmail.com>
2021-09-15 14:23:49 +0200

    Windows added

--- src/main/java/net/atos/bscs211/client/main/windows/Chat.java
+++ src/main/java/net/atos/bscs211/client/main/windows/Chat.java
@@ -1,4 +1,34 @@
 package net.atos.bscs211.client.main.windows;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
+import javafx.event.ActionEvent;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
 public class Chat {
+
+    @FXML private TextField sendMessage;
+    @FXML private Button sendButton;
+    @FXML private Button ausloggenButton;
+
+    @FXML
+    public void sendMessage(ActionEvent event) throws IOException{
+        System.out.println(sendMessage);
+    }
+
+    @FXML
+    public void ausloggenButton(ActionEvent event) throws IOException {
+        URL url = new File("src/main/resources/fxml/login.fxml").toURI().toURL();
+        Parent root = FXMLLoader.load(url);
+        Stage stage = (Stage) ausloggenButton.getScene().getWindow();
+        stage.setScene(new Scene(root, 600, 400));
+    }
+
 }
