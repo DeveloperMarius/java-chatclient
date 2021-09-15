2c0ff4f48003b74f9b7f2e78f498a9b0ff9a53df
Marius Karstedt <m.karstedt@repaste.de>
2021-09-15 11:18:19 +0200

    Added currentUser

--- src/main/java/net/atos/bscs211/client/main/Main.java
+++ src/main/java/net/atos/bscs211/client/main/Main.java
@@ -1,8 +1,11 @@
 package net.atos.bscs211.client.main;
 
+import net.atos.bscs211.objects.User;
 import net.atos.bscs211.utils.DatabaseManager;
 
 public class Main {
+
+    public static User $currentUser = null;
 
     public static void main(String[] args){
         //Do everything
