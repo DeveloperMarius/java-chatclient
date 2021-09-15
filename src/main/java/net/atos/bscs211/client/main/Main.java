255cc40b5898c7cb88deed76f12797fdbb566127
Marius Karstedt <m.karstedt@repaste.de>
2021-09-15 07:48:13 +0200

    Final folder structure

---
+++ src/main/java/net/atos/bscs211/client/main/Main.java
@@ -0,0 +1,8 @@
+package net.atos.bscs211.client.main;
+
+public class Main {
+
+    public static void main(String[] args){
+
+    }
+}
---
+++ src/main/java/net/atos/bscs211/server/main/Main.java
@@ -0,0 +1,8 @@
+package net.atos.bscs211.server.main;
+
+public class Main {
+
+    public static void main(String[] args){
+
+    }
+}
